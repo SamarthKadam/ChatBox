05155a3e0ba3a9b01a9431693b47693d48b2db21
SamarthKadam <samarthskadam14@gmail.com>
2023-09-21 21:40:13 +0530

    pusshed files

--- frontend/src/utils/Reusable.js
+++ frontend/src/utils/Reusable.js
@@ -1,5 +1,6 @@
  export const getSender=(users)=>{
 
     const loggedUser=JSON.parse(localStorage.getItem('info'));
-    return users[0]._id===loggedUser._id?users[1]:users[0];
+    const user=users[0]._id===loggedUser._id?users[1]:users[0];
+    return user;
  }
