6b2bae9be56fb52c01e29756797708490dd75b70
Mastan Sayyad <sayyadmastan78@gmail.com>
2024-05-17 21:29:47 +0530

    Added Link to the Website Logo

--- frontend/src/components/HomeComponents/NavBar.js
+++ frontend/src/components/HomeComponents/NavBar.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import Logo from '../../assets/images/logo-large.png'
 import NavList from './NavList'
 export default function NavBar() {
@@ -6,7 +7,7 @@ export default function NavBar() {
     <div className=' flex justify-between max-[607px]:justify-center'>
         <div className='text-white flex flex-row items-center'>
             <img src={Logo} alt='logo' className='w-10'/>
-            <div className='text-white text-2xl font-Roboto font-semibold ml-2'>ChatBox</div>
+            <div path="/" className='text-white text-2xl font-Roboto font-semibold ml-2'> <Link to="/">ChatBox</Link></div>
         </div>
         <NavList></NavList>
     </div>
