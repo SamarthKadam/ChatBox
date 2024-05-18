14df6edde5b0c3fc59ed879759728c08f957eaff
abhiperfect <abhishekprajapati.890e@gmail.com>
2024-05-18 15:22:38 +0530

    Feat:Improve textarea appearance.

--- frontend/src/components/ChatComponents/Type.js
+++ frontend/src/components/ChatComponents/Type.js
@@ -159,20 +159,37 @@ export default function Type() {
       className="border-[1px] border-[#f5f5f5] bg-[#FFFFFF] h-[12%] flex flex-row justify-center items-center relative"
       ref={emojiPickerRef}
     >
-      {!Microphone && (
-        <div onClick={startListening}>
-          <MicIcon
-            sx={{ width: 38, cursor: "pointer" }}
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "5%",
-              translate: "-4% -50%",
-            }}
-            color="info"
-          ></MicIcon>
-        </div>
+      {!Microphone && !message && (
+        <Box
+          onClick={startListening}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "97%",
+            translate: "-95% -50%",
+          }}
+        >
+          <MicIcon sx={{ width: 22, cursor: "pointer" }} color="info"></MicIcon>
+        </Box>
       )}
+
+      {Microphone && !message && (
+        <Box
+          onClick={stopListening}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "97%",
+            translate: "-95% -50%",
+          }}
+        >
+          <CancelIcon
+            sx={{ width: 22, cursor: "pointer" }}
+            color="info"
+          ></CancelIcon>
+        </Box>
+      )}
+
       <Box
         style={{
           zIndex: 10,
@@ -190,40 +207,35 @@ export default function Type() {
           }}
         />
       </Box>
-
-      {Microphone && (
-        <div onClick={stopListening}>
-          <CancelIcon
-            sx={{ width: 22, cursor: "pointer" }}
-            style={{
-              position: "absolute",
-              top: "50%",
-              left: "4%",
-              translate: "-4% -50%",
-            }}
-            color="info"
-          ></CancelIcon>
-        </div>
-      )}
       <IconButton
         onClick={() => {
           setOpenPicker(!openPicker);
         }}
-      >
-        <InsertEmoticonIcon />
-      </IconButton>
-      <div
-        onClick={sendMessage}
+        sx={{ width: 42, cursor: "pointer" }}
         style={{
           position: "absolute",
           top: "50%",
-          left: "95%",
-          translate: "-95% -50%",
-          cursor: "pointer",
+          left: "2%",
+          translate: "-4% -50%",
         }}
+        color="info"
       >
-        <SendIcon color="action" sx={{ width: 22 }}></SendIcon>
-      </div>
+        <InsertEmoticonIcon />
+      </IconButton>
+      {message && (
+        <Box
+          onClick={sendMessage}
+          style={{
+            position: "absolute",
+            top: "50%",
+            left: "97%",
+            translate: "-95% -50%",
+            cursor: "pointer",
+          }}
+        >
+          <SendIcon color="action" sx={{ width: 22 }}></SendIcon>
+        </Box>
+      )}
       <textarea
         inputRef={inputRef}
         value={message}
@@ -233,7 +245,7 @@ export default function Type() {
         data-gramm="false"
         type="text"
         placeholder="Type a message"
-        className=" bg-gray-100 resize-none font-Roboto box-border max-[1024px]:px-8 px-[5%] flex  text-md max-[900px]:text-sm w-[95%] py-[1%] outline-none h-[70%] rounded-3xl"
+        className="bg-gray-100 resize-none font-Roboto box-border max-[900px]:px-8 px-[5%] flex  text-md max-[900px]:text-sm w-[82%] py-[1%] outline-none h-[70%] rounded-3xl"
       ></textarea>
     </div>
   );
