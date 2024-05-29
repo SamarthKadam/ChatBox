1764f041b74976c7ef67ed762c5985937645feed
surojitmondal <gobindomondal539@gmail.com>
2024-05-29 10:43:21 +0530

    Mic icon fixed, added mic on/off sound, added feature to deactivate Mic after 5 seconds of inactivity

--- frontend/src/components/ChatComponents/Type.js
+++ frontend/src/components/ChatComponents/Type.js
@@ -1,22 +1,20 @@
-import React, { useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import SpeechRecognition, { useSpeechRecognition } from "react-speech-recognition";
+import { socket } from "../../socket/socket";
+
 import MicIcon from "@mui/icons-material/Mic";
 import SendIcon from "@mui/icons-material/Send";
-import { useSelector, useDispatch } from "react-redux";
-import { useState } from "react";
-import { socket } from "../../socket/socket";
-import { AddMessage } from "../../services/Actions/Chat/action";
-import { moveChatToTop } from "../../services/Actions/Chat/action";
-import SpeechRecognition, {
-  useSpeechRecognition,
-} from "react-speech-recognition";
+import { AddMessage, moveChatToTop, updateChatBar } from "../../services/Actions/Chat/action";
 import CancelIcon from "@mui/icons-material/Cancel";
-import { updateChatBar } from "../../services/Actions/Chat/action";
 import data from "@emoji-mart/data";
 import Picker from "@emoji-mart/react";
-import { Box } from "@mui/material";
+import { Box, IconButton } from "@mui/material";
 import useTheme from "@mui/system/useTheme";
-import { IconButton } from "@mui/material";
 import InsertEmoticonIcon from "@mui/icons-material/InsertEmoticon";
+
+import onMicSoundFile from "../../assets/sounds/onMic.mp3";
+import offMicSoundFile from "../../assets/sounds/offMic.mp3";
 
 export default function Type() {
   const isSet = useSelector((state) => state.chat.activeChat);
@@ -27,24 +25,33 @@ export default function Type() {
   const [typing, setTyping] = useState(false);
   const [Microphone, setMircophone] = useState(false);
   const inputRef = useRef(null);
-  const [openPicker, setOpenPicker] = React.useState(false);
+  const [openPicker, setOpenPicker] = useState(false);
   const theme = useTheme();
   const emojiPickerRef = useRef(null);
+  const [noSoundTimeout, setNoSoundTimeout] = useState(false);
+  const [hasMounted, setHasMounted] = useState(false);
 
   const {
     transcript,
-    listening,
     resetTranscript,
     browserSupportsSpeechRecognition,
   } = useSpeechRecognition();
 
   useEffect(() => {
     setMessage(transcript);
-  }, [transcript, resetTranscript]);
+    resetNoSoundTimeout();
+  }, [transcript]);
+
+  const resetNoSoundTimeout = () => {
+    if (noSoundTimeout) {
+      clearTimeout(noSoundTimeout);
+    }
+    setNoSoundTimeout(setTimeout(stopListening, 5000));
+  };
 
   const messageHandler = (e) => {
     setMessage(e.target.value);
-    if (!message) return;
+
     if (!socketConnected) return;
 
     if (!typing) {
@@ -88,13 +95,12 @@ export default function Type() {
   const sendMessage = async (event) => {
     if (message.length === 0) return;
 
-    if ((event.type === "keydown" && event.key === "Enter" && !event.shiftKey) || event.type === "click") {
+    if (event.key === "Enter" || event.type === "click") {
       event.preventDefault();
-      if (!message.trim()) return;
       const cookie = localStorage.getItem("jwt");
       const bodyData = {
         chatId: isSet._id,
-        content: message.trim()
+        content: message,
       };
       setMessage("");
       resetTranscript();
@@ -119,14 +125,31 @@ export default function Type() {
     }
   };
 
+  const onMicSound = () => {
+    const audio = new Audio(onMicSoundFile);
+    audio.play();
+  }
+
+  const offMicSound = () => {
+    const audio = new Audio(offMicSoundFile);
+    audio.play();
+  }
+
   const startListening = () => {
     SpeechRecognition.startListening({ continuous: true, language: "en-IN" });
     setMircophone(true);
+    if (hasMounted) onMicSound();
+    resetNoSoundTimeout();
   };
 
   const stopListening = () => {
     SpeechRecognition.stopListening();
     setMircophone(false);
+    if (hasMounted) offMicSound();
+    if (noSoundTimeout) {
+      clearTimeout(noSoundTimeout);
+      setNoSoundTimeout(null);
+    }
   };
 
   useEffect(() => {
@@ -145,14 +168,17 @@ export default function Type() {
     };
   }, []);
 
+  useEffect(() => {
+    setHasMounted(true);
+  }, []);
+
   function handleEmojiClick(emoji) {
     const input = inputRef.current;
-    // setMessage(emoji)
-    console.log("LL", input);
     if (emoji) {
       setMessage(message + emoji);
     }
   }
+
   if (isSet === null) return <></>;
 
   return (
@@ -167,7 +193,7 @@ export default function Type() {
             style={{
               position: "absolute",
               top: "50%",
-              left: "5%",
+              left: "7%",
               translate: "-4% -50%",
             }}
             color="info"
@@ -199,7 +225,7 @@ export default function Type() {
             style={{
               position: "absolute",
               top: "50%",
-              left: "4%",
+              left: "7.6%",
               translate: "-4% -50%",
             }}
             color="info"
@@ -226,7 +252,7 @@ export default function Type() {
         <SendIcon color="action" sx={{ width: 22 }}></SendIcon>
       </div>
       <textarea
-        inputRef={inputRef}
+        ref={inputRef}
         value={message}
         onKeyDown={sendMessage}
         onChange={messageHandler}
@@ -234,7 +260,7 @@ export default function Type() {
         data-gramm="false"
         type="text"
         placeholder="Type a message"
-        className=" bg-gray-100 resize-none font-Roboto box-border max-[1024px]:px-8 px-[5%] flex  text-md max-[900px]:text-sm w-[95%] py-[1%] outline-none h-[70%] rounded-3xl"
+        className="bg-gray-100 resize-none font-Roboto box-border max-[1024px]:px-8 px-[6%] flex text-md max-[900px]:text-sm w-[95%] py-[1%] outline-none h-[70%] rounded-3xl"
       ></textarea>
     </div>
   );
