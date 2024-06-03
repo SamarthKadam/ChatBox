a8a1efdf8b92092ab7cd958171bb89551aa1e7c7
Ramakrushna Biswal <125277258+RamakrushnaBiswal@users.noreply.github.com>
2024-06-03 22:51:31 +0530

    Removed back to home button in 404Page

--- frontend/src/pages/404Page.js
+++ frontend/src/pages/404Page.js
@@ -5,7 +5,6 @@ export default function NotFoundPage() {
         <>
         <div className=" w-[100%] flex flex-col items-center h-[100vh]">
             <img src={NotfoundPic} alt="404" className="h-[700px] max-[500px]:h-[400px] top-2 filter drop-shadow-2xl "/>
-            <Link to="/" className="bg-[#5080FE] text-white text-xl px-5 py-4 fs-3 rounded-xl bottom-2 mb-16  flex justify-center items-center absolute transition hover:bg-[#0147FF] duration-300 filter drop-shadow-2xl">Back to Home</Link>
         </div>
         </>
     );
