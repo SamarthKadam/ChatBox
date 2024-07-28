6ad9d1fdae048372a149cf925af1b2381f6495dc
balaharisankar <balaharisankar2003@gmail.com>
2024-07-28 18:21:37 +0530

    Gap fixed

--- frontend/src/components/ChatComponents/RecieverMessage.js
+++ frontend/src/components/ChatComponents/RecieverMessage.js
@@ -42,9 +42,9 @@ export default function RecieverMessage({
             ></Avatar>
           </Tooltip>
         )}
-        <div className="bg-[#FFFFFF] max-w-[100%]  rounded-tr-xl ml-[1%] font-Roboto rounded-br-xl rounded-bl-xl box-border px-2 py-2 flex flex-col items-end max-[900px]:text-sm flex justify-between">
+        <div className="bg-[#FFFFFF] max-w-[100%]  rounded-tr-xl ml-[1%] font-Roboto rounded-br-xl rounded-bl-xl box-border px-2 pt-2 pb-2 flex flex-col items-end max-[900px]:text-sm flex justify-between">
           <p className="w-[100%]" style={{wordWrap:"break-word"}}>{content}</p>
-          <p className=" text-[11px] pl-2 pt-1 flex items-end font-medium">{`${String(messageTime.getHours() % 12 || 12).padStart(2, '0')}:${String(messageTime.getMinutes()).padStart(2, '0')} ${messageTime.getHours() >= 12 ? 'pm' : 'am'}`}</p>
+          <p className=" text-[11px] pl-2 flex items-end font-medium">{`${String(messageTime.getHours() % 12 || 12).padStart(2, '0')}:${String(messageTime.getMinutes()).padStart(2, '0')} ${messageTime.getHours() >= 12 ? 'pm' : 'am'}`}</p>
         </div>
       </div>
     </div>
--- frontend/src/components/ChatComponents/SenderMessage.js
+++ frontend/src/components/ChatComponents/SenderMessage.js
@@ -8,7 +8,7 @@ export default function SenderMessage({ time, content }) {
       <div className="flex flex-row justify-end my-1 max-w-[100%] h-auto">
         <div className="bg-[#014DFE] max-w-[100%] rounded-tl-xl font-Roboto rounded-bl-xl rounded-br-xl rouned-bl-xl text-white box-border px-2 py-2 flex flex-col flex-wrap items-end max-[900px]:text-sm mr-2 flex justify-around"> 
           <p className="w-[100%] text-left text-wrap" style={{wordWrap:"break-word"}}>{content}</p>
-          <p className=" text-[11px] pl-2 pt-2 flex items-end font-normal">{`${String(messageTime.getHours()%12 || 12).padStart(2,'0')}:${String(messageTime.getMinutes()).padStart(2,'0')} ${messageTime.getHours()>=12?'pm':'am'}`}</p>
+          <p className=" text-[11px] pl-2 flex items-end font-normal">{`${String(messageTime.getHours()%12 || 12).padStart(2,'0')}:${String(messageTime.getMinutes()).padStart(2,'0')} ${messageTime.getHours()>=12?'pm':'am'}`}</p>
         </div>
       </div>
     </div>
