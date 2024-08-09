a3ac99aec1365442d695d3b6f991a43226c37d4a
Utsavladia <utsavladia@gmail.com>
2024-08-09 21:27:25 +0530

    fixed navbar at top and added hover effect

--- frontend/src/components/HomeComponents/NavBar.js
+++ frontend/src/components/HomeComponents/NavBar.js
@@ -3,7 +3,7 @@ import Logo from '../../assets/images/logo-large.png'
 import NavList from './NavList'
 export default function NavBar() {
   return (
-    <div className=' flex justify-between'>
+    <div className='fixed top-0 left-0 px-40 z-50 bg-[#012478] w-screen flex justify-between'>
         <div className='text-white flex flex-row items-center'>
             <img src={Logo} alt='logo' className='w-10'/>
             <div className='text-white text-2xl font-Roboto font-semibold ml-2'>ChatBox</div>
